dbd4653eda43f250dbed1d6ebf44680643c5c2b8
Hannes Rüger <hannesrueger@gmx.de>
2020-08-04 17:11:56 +0200

    fixed linting errors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
-const packageInfo = require("../package.json");
 import findUp from "find-up";
 import * as fs from "fs";
-import { prompt } from 'enquirer';
+import { prompt } from "enquirer";
 import { program } from "@caporal/core";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageInfo = require("../package.json");
 
 const CONFIG_FILE_NAME = "container-env.json";
 
-export const cli = (argv: string[]) => {
+export function cli(): void {
     program
         .name("container-env")
         .bin("cenv")
@@ -21,7 +23,7 @@ export const cli = (argv: string[]) => {
         .action(async ({ logger, args, options }) => {
             const configFile = await findUp(CONFIG_FILE_NAME);
             if (!configFile || !fs.existsSync(configFile)) {
-                logger.error(`No '${CONFIG_FILE_NAME}' file found. Exiting.`)
+                logger.error(`No '${CONFIG_FILE_NAME}' file found. Exiting.`);
                 return;
             }
             const config = JSON.parse(fs.readFileSync(configFile).toString());
@@ -31,7 +33,7 @@ export const cli = (argv: string[]) => {
             } else if (args.type == "run") {
                 isDockerRun = true;
             } else {
-                logger.error(`Type must be either 'compose' or 'run'. Exiting.`)
+                logger.error("Type must be either 'compose' or 'run'. Exiting.");
                 return;
             }
             const data = isDockerRun ? `docker run ${config.container.scope}/${config.container.name} ${config.variables.map((v: { name: string, default: string }) => `-e ${v.name}=${v.default ? v.default : "CHANGE_ME"}`).join(" ")}
@@ -44,26 +46,28 @@ services:
 ${config.variables.map((v: { name: string, default: string }) => `      - ${v.name}=${v.default ? v.default : "CHANGE_ME"}`).join("\n")}`;
             if (options.save) {
                 const filename = isDockerRun ? "run-in-docker.sh" : "docker-compose.yml";
-                if (!fs.existsSync(filename) || ((await prompt({ name: "result", type: "confirm", message: `'${filename}' already exists. Do you want to overwrite it?` })) as any).result) {   
+                if (!fs.existsSync(filename) || ((await prompt({ name: "result", type: "confirm", message: `'${filename}' already exists. Do you want to overwrite it?` })) as any).result) {
                     fs.writeFileSync(filename, data);
                     return;
                 }
             }
+            // eslint-disable-next-line no-console
             console.log("\n");
+            // eslint-disable-next-line no-console
             console.log(data);
+            // eslint-disable-next-line no-console
             console.log("\n");
-            
         })
-    
+
         .command("init", "Initialize")
-        .action(async ({ logger, args, options }) => {
+        .action(async ({ logger }) => {
             const { container } = await prompt({
                 type: "form",
-                name: 'container',
-                message: 'Please provide the following information:',
+                name: "container",
+                message: "Please provide the following information:",
                 choices: [
-                    { name: 'name', message: 'Container name', initial: '' },
-                    { name: 'scope', message: 'Container scope', initial: '' },
+                    { name: "name", message: "Container name", initial: "" },
+                    { name: "scope", message: "Container scope", initial: "" },
                 ] as any[],
             });
 
@@ -80,30 +84,31 @@ ${config.variables.map((v: { name: string, default: string }) => `      - ${v.na
                         },
                         {
                             name: "number",
-                        }
-                    ]
+                        },
+                    ],
                 })) as any).type == "number";
                 const result = await prompt({
                     type: "form",
-                    name: 'variable',
-                    message: 'Please provide the following information:',
+                    name: "variable",
+                    message: "Please provide the following information:",
                     choices: [
-                        { name: 'name', message: 'Name', initial: '' },
-                        { name: 'description', message: 'Description', initial: '' },
-                        { name: 'default', message: 'Default value', initial: '' }
+                        { name: "name", message: "Name", initial: "" },
+                        { name: "description", message: "Description", initial: "" },
+                        { name: "default", message: "Default value", initial: "" },
                     ] as any[],
                 });
-                (result as any).default = isNumber ? parseInt((result as any).default, 10) : (result as any).default; 
+                (result as any).default = isNumber
+                    ? parseInt((result as any).default, 10)
+                    : (result as any).default;
                 variables.push((result as any).variable);
             }
             fs.writeFileSync(CONFIG_FILE_NAME, JSON.stringify({
                 container,
-                variables
+                variables,
             }));
             logger.info("Done");
-        })
-
+        });
 
     // always run the program at the end
-    program.run()
+    program.run();
 }
